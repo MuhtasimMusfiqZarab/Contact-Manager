17e732a85d9900259b31c31062191f27c0bdb95a
zarab <musfiqzarab@iut-dhaka.edu>
2018-10-13 23:33:37 +0600

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
-import "./App.css";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact
-          name="Muhtasim Musfiq Zarab"
-          email="musfiq.zarab@iut-dhaka.edu"
-          phone="+8801685055749"
-        />
-        <Contact
-          name="Maimuna Haque Mou"
-          email="musfiq.zarab@iut-dhaka.edu"
-          phone="+8801685055749"
-        />
+        <div className="container">
+          <Contact
+            name="Muhtasim Musfiq Zarab"
+            email="musfiq.zarab@iut-dhaka.edu"
+            phone="+8801685055749"
+          />
+          <Contact
+            name="Maimuna Haque Mou"
+            email="musfiq.zarab@iut-dhaka.edu"
+            phone="+8801685055749"
+          />
+        </div>
       </div>
     );
   }
