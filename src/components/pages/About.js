a2068f43613c2391ecb2f9bb6e99874e07edacdf
zarab <musfiqzarab@iut-dhaka.edu>
2018-11-14 20:41:45 +0600

    apply react router

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">ABout Contact Manager</h1>
+      <p className="lead">Simple App To Manage COntacts</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+};
