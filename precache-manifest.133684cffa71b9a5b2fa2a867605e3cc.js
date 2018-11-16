38c58b02b3791ebd2aef21499ba4c368903b2f79
zarab <musfiqzarab@iut-dhaka.edu>
2018-11-16 23:22:43 +0600

    Updates

---
+++ precache-manifest.133684cffa71b9a5b2fa2a867605e3cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dd574dc71532402377",
+    "url": "/Contact-Manager/static/js/runtime~main.65dd574d.js"
+  },
+  {
+    "revision": "b393760e6a2fe924042c",
+    "url": "/Contact-Manager/static/js/main.b393760e.chunk.js"
+  },
+  {
+    "revision": "fce1ee5179595a556591",
+    "url": "/Contact-Manager/static/js/1.fce1ee51.chunk.js"
+  },
+  {
+    "revision": "b393760e6a2fe924042c",
+    "url": "/Contact-Manager/static/css/main.f5741332.chunk.css"
+  },
+  {
+    "revision": "fce1ee5179595a556591",
+    "url": "/Contact-Manager/static/css/1.65cf71b1.chunk.css"
+  },
+  {
+    "revision": "de62474fdb5b2a10343fd79f3f845684",
+    "url": "/Contact-Manager/index.html"
+  }
+];
